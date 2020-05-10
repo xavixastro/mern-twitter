2ce347d78c4dd7ed0ce27a009f3976c59f4632c8
javier <xavixastro@gmail.com>
2020-05-10 11:52:18 -0700

    Backend users and tweets

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World!! :)"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.get("/", (req, res) => res.send("Hello World!! :)"));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
